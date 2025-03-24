dca6c6b16cdc95b8acae1b50104a3d36a4519bdc
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-24 14:09:40 +0700

    fix: image configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: [
+      "cf.bstatic.com",
+      "dynamic-media-cdn.tripadvisor.com",
+      "media-cdn.tripadvisor.com",
+    ],
+  },
 };
 
 export default nextConfig;
