dc1e0d9272cb403254d1b742febd895752ebf796
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-24 14:11:00 +0700

    feat: add logic dashboard ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import DashboardLayout from "@/components/dashboard/dashboard-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if (router.pathname.startsWith("/dashboard")) {
+    return (
+      <DashboardLayout>
+        <Component {...pageProps} />
+      </DashboardLayout>
+    );
+  }
   return <Component {...pageProps} />;
 }
