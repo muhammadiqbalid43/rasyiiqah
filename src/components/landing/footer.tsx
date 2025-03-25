8372bd3c55bedfeb55378f00bef1c721f298b8fa
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-26 06:22:59 +0700

    feat: add footer component

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -1,5 +1,84 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="py-12">Footer</div>;
+  return (
+    <div className="py-12 bg-muted">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-4">
+          <div>
+            <h3>Alamat Kami:</h3>
+            <p className="w-[250px] text-muted-foreground">
+              Jl. Raya Condet No.50, Kel. Batu Ampar, Kramat Jati, Jakarta
+              Timur, DKI Jakarta, Indonesia, 13520
+            </p>
+          </div>
+          <div>
+            <h3>Informasi</h3>
+            <ul>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Metode Pembayaran
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Syarat Ketentuan
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Kebijakan Privasi
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Blog
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Layanan</h3>
+            <ul>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Metode Pembayaran
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="#"
+                  className="text-muted-foreground hover:text-primary"
+                >
+                  Syarat Ketentuan
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>Jam Operasinal</h3>
+            <p>Waktu Operasional</p>
+            <p>Senin - Ahad 09.00 - 18.00 WIB</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
