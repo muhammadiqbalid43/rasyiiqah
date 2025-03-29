2efc1400cbfea3cad6d53d37856a0c52bd2a78cf
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-29 16:20:00 +0700

    feat: add branch type

--- src/types/branch.ts
+++ src/types/branch.ts
@@ -1,12 +1,23 @@
 export interface Branch {
   id: number;
   name: string;
-  imageUrl: string;
+  image: string;
   phone: string;
   address: string;
-  mapUrl: string;
+  map_url: string | string;
 }
 
-export interface BranchResponse {
+export interface CreateBranchFormData {
+  name: string;
+  image: File | null;
+  phone: string;
+  address: string;
+  map_url: string | string;
+}
+
+export interface PaginatedBranches {
   data: Branch[];
+  total: number;
+  page: number;
+  per_page: number;
 }
