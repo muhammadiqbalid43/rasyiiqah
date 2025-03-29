e0da8a82bbc88cade0c26d04f4fd70920e6a5d14
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-29 16:23:52 +0700

    feat: add tanstack query config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import DashboardLayout from "@/components/dashboard/dashboard-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   if (router.pathname.startsWith("/dashboard")) {
     return (
-      <DashboardLayout>
-        <Component {...pageProps} />
-      </DashboardLayout>
+      <QueryClientProvider client={queryClient}>
+        <DashboardLayout>
+          <Component {...pageProps} />
+        </DashboardLayout>
+      </QueryClientProvider>
     );
   }
   return <Component {...pageProps} />;
