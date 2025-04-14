52626b162dcc59c36b64e92d6fbf3370f7d5dcf1
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-04-14 09:41:36 +0700

    feat: update office address

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -37,25 +37,11 @@ const Footer = () => {
                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
-                <span>Jl. Contoh No. 123, Jakarta Pusat, Indonesia</span>
+                <span>
+                  Jl. Sungai Rupat No.6 RT 41 RW 08 Pagar Dewa, Bengkulu
+                </span>
               </li>
-              <li className="flex items-start gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="flex-shrink-0 w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-                <span>info@umrohtravel.com</span>
-              </li>
+
               <li className="flex items-start gap-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -71,7 +57,7 @@ const Footer = () => {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <span>+62 812 3456 7890</span>
+                <span>0821 3666 8000</span>
               </li>
             </ul>
           </div>
